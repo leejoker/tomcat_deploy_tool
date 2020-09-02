3b73b54fcf9ee1813149ef8d1cbd6c0a869b4f05
leejoker <1056650571@qq.com>
2020-09-02 17:41:02 +0800

    add loop listen

--- main.rb
+++ main.rb
@@ -10,14 +10,35 @@ project_path = "#{json['tomcat']}/webapps/#{json['project']}"
 view_path = "#{project_path}#{json['view_target']}"
 lib_path = "#{project_path}#{json['lib_target']}"
 
-puts project_path
-puts view_path
-puts lib_path
+puts "project_path: [#{project_path}]"
+puts "view_path: [#{view_path}]"
+puts "lib_path: [#{lib_path}]"
 
-puts '**************************************部署jar包*********************************************'
+# create metadata file
 fo = FileOperator.new
-jar_files = fo.get_files_by_rule(json['lib_src'])
-fo.copy_files_to_dir(jar_files, lib_path)
+exist = File.exist?('metadata')
+fo.create_metadata unless exist
 
-puts '**************************************部署views********************************************'
-fo.copy_dir(json['view_src'], view_path)
+jar_files = fo.get_files_by_rule(json['lib_src'])
+
+loop do
+  update_array = []
+  hash_array = {}
+  jar_files.each do |file|
+    file_name = File.basename(file, '.jar')
+    time = fo.get_metadata_value(file_name)
+    stat = File::Stat.new(file)
+    mtime = stat.mtime.to_i
+    unless time.nil?
+      update_array.push(file) if mtime > time.to_i
+    end
+    hash_array[file_name] = stat.mtime.to_i
+  end
+  fo.write_metadata_value(hash_array)
+  puts '**************************************部署jar包*********************************************'
+  fo.copy_files_to_dir(update_array, lib_path)
+  puts '**************************************部署views********************************************'
+  fo.copy_dir(json['view_src'], view_path) unless update_array.empty?
+
+  sleep 30
+end
--- utils/file_operations.rb
+++ utils/file_operations.rb
@@ -21,4 +21,51 @@ class FileOperator
     puts "copy [#{src}] to [#{dest}]"
     FileUtils.cp_r src, dest
   end
+
+  def create_metadata
+    file = File.new('metadata', 'w+')
+    file.puts('# time metadata')
+    file.close
+  end
+
+  def get_metadata_value(key)
+    lines = IO.readlines('metadata')
+    lines.shift
+    lines.each do |line|
+      if line.include?(key)
+        value_array = line.split('=')
+        return value_array[1]
+      end
+    end
+    nil
+  end
+
+  def write_metadata_value(hash_value)
+    line_array = []
+    lines = IO.readlines('metadata')
+    update_by_hash(lines, hash_value, line_array)
+    unless hash_value.empty?
+      hash_value.each do |key, value|
+        line_array.push("#{key}=#{value}")
+      end
+    end
+    write_lines_from_start('metadata', line_array)
+  end
+
+  def update_by_hash(lines, hash, line_array)
+    lines.each do |line|
+      value_array = line.split('=')
+      line = "#{value_array[0]}=#{hash[value_array[0]]}" unless hash[value_array[0]].nil?
+      line_array.push(line)
+      hash.delete(value_array[0])
+    end
+  end
+
+  def write_lines_from_start(file, lines)
+    f = File.open(file, 'w+')
+    lines.each do |line|
+      f.puts line
+    end
+    f.close
+  end
 end
